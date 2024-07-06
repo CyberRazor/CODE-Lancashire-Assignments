9a2a1fa5085770e22c65568c9245e54c2d562021
CyberRazor <stomp224@gmail.com>
2024-07-06 23:46:01 +0100

    updated sega innerHTML

--- Website/sega.js
+++ Website/sega.js
@@ -2,11 +2,11 @@ var imgS = document.getElementById('SonicIMG')
 var txtS = document.getElementById('SonicTXT')
 
 imgS.addEventListener("mouseout", function() {
-    txtS.textContent = "Sonic The Hedgehog"
+    txtS.innerHTML = "Sonic The Hedgehog"
 })
 
 imgS.addEventListener("mouseover", function() {
-    txtS.textContent = "Action Platformer. Released on the Sega Mega Drive, 1991"
+    txtS.innerHTML = "Action Platformer. Released on the Sega Mega Drive, 1991"
 })
 
 
@@ -14,11 +14,11 @@ var imgN = document.getElementById('NightsIMG')
 var txtN = document.getElementById('NightsTXT')
 
 imgN.addEventListener("mouseout", function() {
-    txtN.textContent = "Nights Into Dreams"
+    txtN.innerHTML = "Nights Into Dreams"
 })
 
 imgN.addEventListener("mouseover", function() {
-    txtN.textContent = "Action Adventure. Released on the Sega Saturn, 1996"
+    txtN.innerHTML = "Action Adventure. Released on the Sega Saturn, 1996"
 })
 
 
@@ -26,9 +26,9 @@ var imgO = document.getElementById('OutrunIMG')
 var txtO = document.getElementById('OutrunTXT')
 
 imgO.addEventListener("mouseout", function() {
-    txtO.textContent = "Outrun"
+    txtO.innerHTML = "Outrun"
 })
 
 imgO.addEventListener("mouseover", function() {
-    txtO.textContent = "Driving simulator. Released in arcades, 1986"
+    txtO.innerHTML = "Driving simulator. Released in arcades, 1986"
 })
